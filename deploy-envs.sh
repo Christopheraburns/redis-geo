d35dc5378146c41c4bc61569cd969bdfea98e68e
Chris Burns <christopheraburns@gmail.com>
2016-11-22 18:31:39 -0600

    build 0.0.7
    latest

--- deploy-envs.sh
+++ deploy-envs.sh
@@ -6,7 +6,7 @@ export IMAGE_VERSION=latest
 
 export AWS_DEFAULT_REGION=us-west-2
 export AWS_ECS_CLUSTER_NAME=default
-export AWS_VIRTUAL_HOST=35.164.17.31
+export AWS_VIRTUAL_HOST=35.163.8.102
 
 # set any sensitive information in travis-ci encrypted project settings:
 # required: AWS_ACCOUNT_NUMBER, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
